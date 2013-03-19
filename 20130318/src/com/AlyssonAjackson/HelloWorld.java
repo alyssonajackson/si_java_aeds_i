a3667c537fdc1e7c63b16bd43ebf51ea3a46aad8
Alysson Ajackson <dev@alysson.net>
2013-03-18 21:51:02 -0300

    first project: hello world

---
+++ 20130318/src/com/AlyssonAjackson/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.AlyssonAjackson;
+
+/**
+ * @author 11312399
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+	}
+
+}
