eeb8f5f348a20378dbf905a5f1a479b57ee6bde1
Alysson Ajackson <dev@alysson.net>
2013-04-01 21:48:48 -0300

    20130401::exmplo1

---
+++ 20130318/src/com/AlyssonAjackson/AloGalera.java
@@ -0,0 +1,21 @@
+package com.AlyssonAjackson;
+
+import javax.swing.JOptionPane;
+
+public class AloGalera {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try{
+			JOptionPane.showMessageDialog(null, "Alô Galera");
+		}catch(Exception e){
+			System.out.println("Erro encontrado.");
+		}
+
+	}
+
+}
