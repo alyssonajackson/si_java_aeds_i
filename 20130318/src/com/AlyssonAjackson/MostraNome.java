b3fa523449b678eb371be07f5de367c712f9bbf2
Alysson Ajackson <dev@alysson.net>
2013-04-01 21:52:46 -0300

    20130401::exmplo2

---
+++ 20130318/src/com/AlyssonAjackson/MostraNome.java
@@ -0,0 +1,21 @@
+package com.AlyssonAjackson;
+
+import java.io.*;
+import javax.swing.JOptionPane;
+
+public class MostraNome {
+
+	public static void main(String[] args) {
+		
+		String nome;
+		nome = JOptionPane.showInputDialog("Digite seu nome: ");
+		
+		try{
+			JOptionPane.showMessageDialog(null, "O nome digitado foi: " + nome);
+		}catch(Exception e){
+			System.out.println("Erro encontrado.");
+		}
+		
+	}
+	
+}
